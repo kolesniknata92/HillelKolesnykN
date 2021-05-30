45dc333c1c38716380dddc8d205810a81fa81c2a
Nataliya <kolesnyk.natalia@techs.com.ua>
2021-05-30 10:44:48 +0300

    Home Work 5 сommit

--- src/Lesson6HomeWork/PhoneBill.java
+++ src/Lesson6HomeWork/PhoneBill.java
@@ -2,7 +2,7 @@ package Lesson6HomeWork;
 
 public class PhoneBill {
     public static void main(String[] args) {
-        MobileOperatorTariff mobileOperatorTariff = new MobileOperatorTariff(10.0, 160,
+        MobileOperatorTariff mobileOperatorTariff = new MobileOperatorTariff(10.0, 550,
                 10, 12, 100);
         System.out.println("Стоимость израсходованного пакета за месяц: " + mobileOperatorTariff.getPhoneBill());
 
