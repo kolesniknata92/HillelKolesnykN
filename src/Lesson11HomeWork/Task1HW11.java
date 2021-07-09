6b61fb9727f4ac23b44e10a664a0becd9f56d1d8
Nataliya <kolesnyk.natalia@techs.com.ua>
2021-07-09 15:49:02 +0300

    Home Work 8 сommit

---
+++ src/Lesson11HomeWork/Task1HW11.java
@@ -0,0 +1,26 @@
+package Lesson11HomeWork;
+
+import java.util.Locale;
+
+public class Task1HW11 {
+
+    public static void main(String[] args) {
+
+        String[] arrayString = "Get a modern feel with a smudge-resistant only for $2.50, you can finish now".split ("\\s");
+        String with = arrayString[4];
+        System.out.println(with.toUpperCase(Locale.ROOT));
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
